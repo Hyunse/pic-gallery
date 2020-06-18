4157f2ff0af55ba8d156ab1bae665fc5e7e426d6
Hyunse Kim <anzmf12@naver.com>
2020-06-18 01:05:32 -0400

    Add search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,12 +5,22 @@ import spaceImg from '../resource/space.jpg';
 class Search extends Component {
   constructor(props) {
     super(props);
-    this.state = {  };
+    this.state = {};
   }
   render() {
     return (
       <div className="search-container">
         <img src={spaceImg} alt="search-back" />
+        <div className="search-bar-area">
+          <span>Unsplash API</span>
+          <div>
+            <input
+              type="text"
+              className="search-bar search-bar-text"
+              placeholder="Search Images"
+            />
+          </div>
+        </div>
       </div>
     );
   }
