1493d7f659b4057aaddef855a0ffce90ab0ba3a8
Hyunse <anzmf12@naver.com>
2020-06-19 06:24:35 +0000

    Deploying to gh-pages from  @ ac72dac241adfd72e6c00ab82839e1e9f40dd16b 🚀

---
+++ precache-manifest.988622c4e242b32adbc54124781315b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5aebf28b643aa30897eeda5a43b14d7",
+    "url": "/pic-map/index.html"
+  },
+  {
+    "revision": "8e51a0f0e77800284053",
+    "url": "/pic-map/static/css/main.c4437eed.chunk.css"
+  },
+  {
+    "revision": "902ffc499b7149c954a6",
+    "url": "/pic-map/static/js/2.c621294f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pic-map/static/js/2.c621294f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e51a0f0e77800284053",
+    "url": "/pic-map/static/js/main.879e9c9a.chunk.js"
+  },
+  {
+    "revision": "56a32e246506858ed5a8",
+    "url": "/pic-map/static/js/runtime-main.7b4367e7.js"
+  },
+  {
+    "revision": "bde89f9089f5fa7762331e4b4a924f5d",
+    "url": "/pic-map/static/media/space.bde89f90.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pic-map/precache-manifest.653056a69380de341822f46da1c1b023.js"
+  "/pic-map/precache-manifest.988622c4e242b32adbc54124781315b2.js"
 );
 
 self.addEventListener('message', (event) => {
